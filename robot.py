cfa647daa1b81d7112de0965629076026141991e
callum <Callumr714@icloud.com>
2023-11-03 13:25:52 +0000

    Robot.py init commit

---
+++ robot.py
@@ -0,0 +1,9 @@
+from sr.robot3 import *
+
+'''
+This script is the one the robot ultimately runs, so load all scripts needed into here
+
+vision.py - Controls Robots camera and marker finding ability - Author Callum R
+
+'''
+robot = Robot()
