027ad9a28345ca0e8e7fc733a0b8ce96cf823024
callum <Callumr714@icloud.com>
2023-11-06 19:54:49 +0000

    Added Board Values

--- robot.py
+++ robot.py
@@ -16,6 +16,11 @@ power = 0.5
 
 stopping_distance = 150 #mm, distance from marker robot should stop
 
+# -- boards --
+motor_board = robot.motor_board
+power_board = robot.power_board
+robot.power_board.outputs[OUT_H0].is_enabled = True
+
 # -- vision prereq --
 current_markers = [] # what markers the robot can currently see
 # -- main run loop --
@@ -27,4 +32,5 @@ while True:
     movement_values - [angle (in degrees), distance] to nearest marker. If no markers, this is empty
     '''
     movement_values = vision.vision_run(robot, False, dev)
-    print(movement_values)
+    if movement_values: # check if not empty
+        duration = movement.angle_to_duration(movement_values[0])
