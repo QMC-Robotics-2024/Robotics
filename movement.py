4afb966d9bb4808c68b383681ebe673c90d48bb7
callum <Callumr714@icloud.com>
2023-11-09 12:58:34 +0000

    Better movement.py

--- movement.py
+++ movement.py
@@ -15,26 +15,33 @@ def angle_to_duration(angle, thresh):
     if angle <= thresh:
         duration = 0
     else:
-        duration = angle * (0.65/90)
+        duration = angle * (0.85/90)
     return abs(duration)
 
+def rotate_check(angle, thresh):
+    if angle >= thresh:
+        return 1
+    elif angle <= -thresh:
+        return -1
+    else:
+        return 0
 
 # need to be modified once the distance-power conversion is found
 def forward(board, power=0.5):
-    board.motors[0].power = power
+    board.motors[0].power = -power
     board.motors[1].power = power
 
 
 def reverse(board, power=0.5):
     board.motors[0].power = power
-    board.motors[1].power = power
+    board.motors[1].power = -power
 
 def turn_clockwise(robot, power=0.5): # adapted from Jack's script
     robot.motor_board.motors[0].power = -power
-    robot.motor_board.motors[1].power = power
+    robot.motor_board.motors[1].power = -power
 def turn_anticlockwise(robot, power=0.5):
     robot.motor_board.motors[0].power = power
-    robot.motor_board.motors[1].power = -power
+    robot.motor_board.motors[1].power = power
 
 def stop(board):
     board.motors[0].power = 0
