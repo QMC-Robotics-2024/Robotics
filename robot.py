2c2c44f0c3f896bae9e8d1517255c246ca321955
callum <Callumr714@icloud.com>
2023-11-22 20:54:34 +0000

    Fixes

--- robot.py
+++ robot.py
@@ -1,7 +1,7 @@
 from sr.robot3 import *
 
 from Modules import vision
-from Modules import movement as motion
+from Modules import adapted_movement as motion
 from Modules import behaviour
 
 print("Script Running")
@@ -16,19 +16,19 @@ robot = Robot()
 dev = True # developer mode
 
 power = 0.5
-rotat_power = 0.2
+rotat_power = 0.5
 speed = 0 # speed in mps of robot
 
-stopping_distance = 500 #mm, distance from marker robot should stop
+stopping_distance = 300 #mm, distance from marker robot should stop
 
 
-angle_thresh = 0.05# threshold for angle
+angle_thresh = 0.01# threshold for angle
 
 exposure_values = [x for x in range(-13, 0)] # camera settings
 
-scan_duration = 1 # how long it turns for when scanning
-check_duration = 2 # how long it checks for
-rotate_incremen_speed = 0.5
+scan_duration = 0.5 # how long it turns for when scanning
+check_duration = 0.4 # how long it checks for
+rotate_incremen_speed = 0.3
 
 # -- boards --
 motor_board = robot.motor_board
@@ -38,6 +38,7 @@ robot.power_board.outputs[OUT_H0].is_enabled = True
 # -- vision prereq --
 current_markers = [] # what markers the robot can currently see
 # -- main run loop --
+behaviour.set_motion(motion)
 while True:
     '''
     Variables:
@@ -46,31 +47,49 @@ while True:
     # get inital values
     movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
     id = target_marker.id
-    if movement_values: # check if not empty
+    if movement_values:
+        print("Inital Values Stated")# check if not empty
         while True:
             distance = 0
             angle = 0
             try:
                 # try finding the distance, if it cant see it, set distance and angle to 0
                 distance, angle = vision.distance_update(robot, id)
-            except:
+                print("Found Values from inital")
+            except TypeError:
                 # this stops it if it cant find any asteroids, so it can search without blur
-                pass
+                print("No Values Found")
             if not distance and not angle:
+                print("Starting Check")
+                motion.stop(motor_board)
                 robot.sleep(check_duration)
                 try:
                     distance, angle = vision.distance_update(robot, id)
-                except:
+                    print("Values found for check")
+                except TypeError:
                     # it can no longer find the marker, so update it
-                    movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
-                    while not current_markers:
-                        current_markers = behaviour.scan_for_markers(robot, rotate_incremen_speed, scan_duration, check_duration)
-                    movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
-                    id = target_marker.id
-                    distance, angle = vision.distance_update(robot, id)
-            behaviour.turn_to_marker(motor_board, rotat_power, angle, angle_thresh)
-            behaviour.drive_to_marker(motor_board, power, distance, stopping_distance)
-
+                    print("Lost Marker, Updating")
+                    try:
+                        movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
+                        distance, angle = vision.distance_update(robot, target_marker.id)
+                    except TypeError:
+                        print("Beginning Scan")
+                        current_markers = False
+                        while not current_markers:
+                            print("Scanning")
+                            current_markers = behaviour.scan_for_markers(robot, rotate_incremen_speed, scan_duration, check_duration)
+                        movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
+                        id = target_marker.id
+                        distance, angle = vision.distance_update(robot, id)
+                        print("Found Values")
+            if distance > stopping_distance:
+                behaviour.turn_to_marker(motor_board, rotat_power, angle, angle_thresh)
+                behaviour.drive_to_marker(motor_board, power, distance, stopping_distance)
+            else:
+                motion.stop(motor_board)
+    elif not movement_values:
+        while not current_markers:
+            current_markers = behaviour.scan_for_markers(robot, rotate_incremen_speed, scan_duration, check_duration)
 
 """
 duration = movement.angle_to_duration(movement_values[0], angle_thresh)
