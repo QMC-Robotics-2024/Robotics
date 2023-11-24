6a058998b2c021b45c24e1b60418726860433231
callum <Callumr714@icloud.com>
2023-11-24 13:31:36 +0000

    Ultrasonic Drive

--- robot.py
+++ robot.py
@@ -11,7 +11,6 @@ vision.py - Controls Robots camera and marker finding ability - Author Callum R
 
 '''
 # -- constants --
-robot = Robot()
 dev = True  # developer mode
 
 power = 0.5
@@ -23,12 +22,18 @@ angle_thresh = 0.01  # threshold for angle
 
 scan_duration = 0.5  # how long it turns for when scanning
 check_duration = 0.4  # how long it checks for
-rotate_incremen_speed = 0.3
+rotate_increment_speed = 0.3
 
+arduino_speed = 0.3
+arduino_min = 200 # distance in mm arduino stops robot
 # -- boards --
+# replace with our boards number
+robot = Robot()
 motor_board = robot.motor_board
 power_board = robot.power_board
 robot.power_board.outputs[OUT_H0].is_enabled = True
+arduino = robot.arduino # using extended SR firmware (no reason really tbh)
+
 
 # -- vision prereq --
 current_markers = []  # what markers the robot can currently see
@@ -74,7 +79,7 @@ while True:
                         current_markers = False
                         while not current_markers:
                             print("Scanning")
-                            current_markers = behaviour.scan_for_markers(robot, rotate_incremen_speed, scan_duration,
+                            current_markers = behaviour.scan_for_markers(robot, rotate_increment_speed, scan_duration,
                                                                          check_duration)
                         movement_values, target_marker, current_markers = vision.vision_run(robot, False, dev)
                         id = target_marker.id
@@ -83,12 +88,14 @@ while True:
             if distance > stopping_distance:
                 behaviour.turn_to_marker(motor_board, rotat_power, angle, angle_thresh)
                 behaviour.drive_to_marker(motor_board, power, distance, stopping_distance)
-
+            elif distance == stopping_distance or distance < stopping_distance:
+                print("[ARDUINO ACTIVE]")
+                behaviour.ultrasonic_drive(motor_board, arduino_speed, arduino, arduino_min)
             else:
                 motion.stop(motor_board)
     elif not movement_values:
         while not current_markers:
-            current_markers = behaviour.scan_for_markers(robot, rotate_incremen_speed, scan_duration, check_duration)
+            current_markers = behaviour.scan_for_markers(robot, rotate_increment_speed, scan_duration, check_duration)
 
 """
 duration = movement.angle_to_duration(movement_values[0], angle_thresh)
