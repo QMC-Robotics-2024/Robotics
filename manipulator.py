6fd35a3b19e1ad46f688cc3f5f59cbacd871d75f
Hackercat <111284182+Hackercat77@users.noreply.github.com>
2023-11-27 13:03:08 +0000

    Tweaks

---
+++ manipulator.py
@@ -0,0 +1,8 @@
+'''
+Program to control the mechanism used for manipulating boxes (Asteroids/Egg)
+'''
+#----------Imports----------#
+# Import student robotics library
+from sr.robot3 import *
+
+#----------Subroutines----------#
