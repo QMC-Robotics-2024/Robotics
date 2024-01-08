ad0eb0469c9176bab0338cb2f99444869d36d48b
Hackercat <111284182+Hackercat77@users.noreply.github.com>
2024-01-08 12:19:32 +0000

    Typography

--- Modules/manipulator.py
+++ Modules/manipulator.py
@@ -1,10 +1,11 @@
 '''
 Program to control the mechanism used for manipulating boxes (Asteroids/Egg)
-Program controls suction and raising and lowerting of the mechanism
+Program controls suction and raising and lowering of the mechanism
 
 If you have issues with this dm @hackercat77 or tag me on the discord. Odds are something stupid is eff'ed and I need to fix it
 
 Currently waiting for the build team to get their sh*t together so i can finish writing this
+(Who in the name of king Charles left bollock put a normal motor on something that requires precise control when there is a box of eff'in servos!!!!)
 
 Author: JP
 
@@ -27,7 +28,7 @@ def suction_off(board):
 def raise_arm():
     # Raises manipulator mechanism (arm was shorter to type)
     '''
-    this will use the servo controler board to raise the arm by a certain factor that will be determined by how high the box needsto be raised
+    this will use the servo controller board to raise the arm by a certain factor that will be determined by how high the box needsto be raised
     e.g. to raise high enough to place box in a "spaceship"
     '''
     pass
